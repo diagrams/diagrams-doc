6f87d342781a58d6947448eae507c62290acdd29
Brent Yorgey <byorgey@cis.upenn.edu>
2011-05-21 18:58:38 +0000

    a bunch of work on the gallery (not done yet)

--- web/gallery.markdown
+++ web/gallery.markdown
@@ -2,4 +2,7 @@
 title: Gallery
 ---
 
-<p align=center>Coming soon!</p>
+A collection of some of the best examples constructed with
+diagrams.  Click on a diagram to see its source code.
+
+<p><hr /></p>
--- web/hakyll.hs
+++ web/hakyll.hs
@@ -1,37 +1,84 @@
 {-# LANGUAGE OverloadedStrings
            , NoMonomorphismRestriction
    #-}
-import Control.Arrow ((>>>), arr)
+import Control.Arrow ((>>>), (>>^), (^>>), arr, second)
 import Control.Monad (forM_)
+import Data.List (sortBy)
+import Data.Ord (comparing)
+import Data.Monoid
 
 import Data.String
+
+import System.FilePath
 
 import Hakyll
 
 main :: IO ()
 main = hakyll $ do
+    -- CSS, templates -----------------------------
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
 
     match "templates/*" $ compile templateCompiler
 
-    match "images/*" $ do
+    -- Normal .html pages, built from .markdown ---
+    forM_ pages $ flip match $ do
+        route   $ setExtension "html"
+        compile $ pageCompiler >>> mainCompiler
+
+    -- Example gallery ----------------------------
+
+      -- make all gallery/*.lhs available for building gallery.html
+    match "gallery/*.lhs" $ compile (pageCompiler >>^ (setHtmlURL . setImgURL))
+
+      -- build gallery.html from gallery.markdown and gallery/*.lhs
+      -- note the inGroup Nothing, which ensures we don't get
+      -- gallery/*.lhs resources from other groups, since we are using
+      -- gallery/*.lhs to build several different things
+    match "gallery.markdown" $ do
+        route $ setExtension "html"
+
+        compile $ pageCompiler
+            >>> requireAllA ("gallery/*.lhs" `mappend` inGroup Nothing) buildGallery
+            >>> mainCompiler
+
+      -- export .png files
+    match "gallery/*.png" $ do
         route idRoute
         compile copyFileCompiler
 
-    forM_ pages $ \page ->
-        match page $ do
-            route   $ setExtension "html"
-            compile $ pageCompiler
-                >>> applyTemplateCompiler "templates/default.html"
-                >>> relativizeUrlsCompiler
+      -- build syntax-highlighted source code for examples
+    group "gallery" $ match "gallery/*.lhs" $ do
+        route $ setExtension "html"
+        compile $ pageCompiler
+            >>> arr setImgURL
+            >>> applyTemplateCompiler "templates/exampleHi.html"
+            >>> mainCompiler
 
-    group "raw" $ forM_ lhs $ \lhs ->
-        match lhs $ do
-            route idRoute
-            compile (readPageCompiler >>> arr pageBody)
+      -- export raw .lhs of examples for download, stripping off the
+      -- metadata first
+    group "raw" $ forM_ lhs $ flip match $ do
+        route idRoute
+        compile (readPageCompiler >>^ pageBody)
 
+mainCompiler = applyTemplateCompiler "templates/default.html"
+           >>> relativizeUrlsCompiler
+
+setImgURL    = setURL "png"
+setHtmlURL   = setURL "html"
+setURL ext p = trySetField (ext ++ "url") (replaceExtension (getField "url" p) ext) p
+
+buildGallery :: Compiler (Page String, [Page String]) (Page String)
+buildGallery = second (mapCompiler compileExample >>> sortDate >>> arr (map pageBody))
+               >>> arr (\(main, exs) -> modBody (++ (concat exs)) main)
+  where sortDate = arr (sortBy $ flip (comparing (getField "date")))
+
+compileExample :: Compiler (Page String) (Page String)
+compileExample = applyTemplateCompiler "templates/example.html"
+
+modBody :: (a -> b) -> Page a -> Page b
+modBody f p = p { pageBody = f (pageBody p) }
 
 pages = md ++ lhs
 
@@ -39,8 +86,7 @@ md = map (fromString . (++".markdown"))
   [ "index"
   , "download"
   , "documentation"
-  , "gallery"
   , "community"
   ]
 
-lhs = [ "tutorial/DiagramsTutorial.lhs" ]
+lhs = [ "tutorial/DiagramsTutorial.lhs", "gallery/*.lhs" ]
