27dab2d91aeab013cf257a7c9417504301bdc814
Brent Yorgey <byorgey@cis.upenn.edu>
2011-07-29 20:25:40 +0000

    my directory structure changed

--- logs/run-hog.sh
+++ logs/run-hog.sh
@@ -1,1 +1,1 @@
-hog --host='irc.freenode.org' --port=6667 --channels='#diagrams' --logpath=/home1/b/byorgey/hacking/diagrams/doc/logs --nick=diagrams-hog --user=diagrams-hog
+hog --host='irc.freenode.org' --port=6667 --channels='#diagrams' --logpath=/home1/b/byorgey/src/diagrams/doc/logs --nick=diagrams-hog --user=diagrams-hog
