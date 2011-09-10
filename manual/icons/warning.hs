803a6c1ed4a463c53a050c448f896781d1996df2
Brent Yorgey <byorgey@cis.upenn.edu>
2011-09-10 16:03:21 +0000

    make warning sign yellow + black

--- manual/icons/warning.hs
+++ manual/icons/warning.hs
@@ -7,7 +7,8 @@ signSide = hrule 1 <> arc (-1/4 :: CircleFrac) ((-1/4) + 1/3) # scale 0.3
 
 sign = mconcat . take 3 . iterate (rotateBy (1/3)) $ signSide
 
-d = strokeT sign # lc red # lw 0.1 # centerXY 
- <> text "!" # fc red # bold
+d = text "!" # fc black # bold
+ <> strokeT sign # lc black # fc yellow # lw 0.1 # centerXY 
+ 
 
 main = defaultMain $ pad 1.15 d
