ef4cfc82aa0ce08d086942ac9e0edc9ecfb5cf84
Brent Yorgey <byorgey@cis.upenn.edu>
2012-10-02 08:43:09 -0400

    add web preview mode to Shake.hs

--- Shake.hs
+++ Shake.hs
@@ -1,53 +1,68 @@
-import System.Directory
+{-# LANGUAGE DeriveDataTypeable #-}
+
 import Development.Shake
 import Development.Shake.FilePath
+import System.Console.CmdArgs
+import System.Directory
 
 obj = (".make" </>)
 un = dropDirectory1
 
 dist = ("dist" </>)
 
+data MkMode =
+    Build
+  | Preview
+  deriving (Show, Data, Typeable)
+
+mkModes = modes
+  [ Build &= auto
+  , Preview
+  ]
+
 main :: IO ()
-main = shake shakeOptions { shakeThreads = 2 } $ do
-  want [dist "manual/diagrams-manual.html"]
-  action $ requireIcons
-  action $ requireStatic
+main = do
+  m <- cmdArgs mkModes
+  shake shakeOptions { shakeThreads = 2 } $ do
+    want [dist "manual/diagrams-manual.html"]
+    action $ requireIcons
+    action $ requireStatic
 
-  webRules
-  action $ buildWeb
+    webRules
+    action $ runWeb m
 
-  -- Cheating a bit here; this rule also generates diagrams images,
-  -- BUT we don't even know their names until after running xml2html,
-  -- which generates diagrams-manual.html as well.  So we have to be
-  -- careful to call 'requireImages' *after* running xml2html.
-  dist "manual/diagrams-manual.html" *> \out -> do
-    let xml = obj . un $ replaceExtension out "xml"
-        exe = obj "manual/xml2html.hs.exe"
-    need [xml, exe]
-    system' exe [xml, "-o", obj "manual/images", out]
-    requireImages
+    -- Cheating a bit here; this rule also generates diagrams images,
+    -- BUT we don't even know their names until after running xml2html,
+    -- which generates diagrams-manual.html as well.  So we have to be
+    -- careful to call 'requireImages' *after* running xml2html.
+    dist "manual/diagrams-manual.html" *> \out -> do
+      let xml = obj . un $ replaceExtension out "xml"
+          exe = obj "manual/xml2html.hs.exe"
+      need [xml, exe]
+      system' exe [xml, "-o", obj "manual/images", out]
+      requireImages
 
-  obj "//*.xml" *> \out -> do
-    let rst = un $ replaceExtension out "rst"
-    need [rst]
-    system' "rst2xml" ["--input-encoding=utf8", rst, out]
+    obj "//*.xml" *> \out -> do
+      let rst = un $ replaceExtension out "rst"
+      need [rst]
+      system' "rst2xml" ["--input-encoding=utf8", rst, out]
 
-  obj "//*.hs.exe" *> \out -> do
-    let hs = un $ dropExtension out
-    need [hs]
-    ghc out hs
+    obj "//*.hs.exe" *> \out -> do
+      let hs = un $ dropExtension out
+      need [hs]
+      ghc out hs
 
-  dist "manual/icons/*.png" *> \out -> do
-    let exe = obj . un $ replaceExtension out ".hs.exe"
-    need [exe]
-    system' exe ["-w", "40", "-h", "40", "-o", out]
+    dist "manual/icons/*.png" *> \out -> do
+      let exe = obj . un $ replaceExtension out ".hs.exe"
+      need [exe]
+      system' exe ["-w", "40", "-h", "40", "-o", out]
 
-  copyFiles "manual/static"
-  copyImages
+    copyFiles "manual/static"
+    copyImages
 
-  addOracle ["ghc-pkg"] $ do
-    (out,_) <- systemOutput "ghc-pkg" ["list","--simple-output"]
-    return $ words out
+    addOracle ["ghc-pkg"] $ do
+      (out,_) <- systemOutput "ghc-pkg" ["list","--simple-output"]
+      return $ words out
 
 copyFiles dir = dist (dir ++ "/*") *> \out -> copyFile' (un out) out
 
@@ -76,21 +91,29 @@ webRules = do
   "web/manual" *> \out ->
     system' "ln" ["-s", "-f", "../dist/manual", out]
 
-buildWeb :: Action ()
-buildWeb = do
+runWeb :: MkMode -> Action ()
+runWeb m = do
   alwaysRerun
+  needWeb
 
+  systemCwd "web" (".." </> obj "web/hakyll.hs.exe")
+    [ case m of
+        Build   -> "build"
+        Preview -> "preview"
+    ]
+
+needWeb :: Action ()
+needWeb = do
   need [ "web/manual"
        , dist "manual/diagrams-manual.html"
        , obj "web/hakyll.hs.exe"
-       , obj "web/gallery/Build.hs.exe"]
+       , obj "web/gallery/Build.hs.exe"
+       ]
   requireIcons
   requireStatic
   requireImages
 
-  systemCwd "web" (".." </> obj "web/hakyll.hs.exe") ["build"]
-
-  -- action $ requireGallery  -- replace hakyll building with our own here
+  -- requireGallery  -- replace hakyll building with our own here
 
 ghc out hs = do
   askOracle ["ghc-pkg"]
