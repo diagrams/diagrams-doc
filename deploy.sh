8a1bdd4fbfc380b7313934ec4a00ade3125d325a
Brent Yorgey <byorgey@cis.upenn.edu>
2012-12-27 13:53:09 -0500

    fix wrong directory in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 ghc --make Shake
 ./Shake build
-rsync -r _site/* byorgey@projects.haskell.org:/srv/projects/diagrams/
+rsync -r web/_site/* byorgey@projects.haskell.org:/srv/projects/diagrams/
 ssh byorgey@projects.haskell.org 'chmod -R o+rX /srv/projects/diagrams/*'
