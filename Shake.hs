9b5081d83780a84fa6749fe6b83eba21d49723fd
Brent Yorgey <byorgey@cis.upenn.edu>
2013-08-02 15:26:19 -0400

    almost finished porting to hakyll 4

--- Shake.hs
+++ Shake.hs
@@ -2,11 +2,16 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 import           Control.Parallel.Strategies (NFData)
+import           Data.Functor                ((<$>))
+import           Data.List                   (isPrefixOf)
 import           Development.Shake
 import           Development.Shake.Classes   (Binary, Hashable)
 import           Development.Shake.FilePath  (dropDirectory1, dropExtension,
-                                              takeDirectory, (-<.>), (</>))
+                                              takeBaseName, takeDirectory,
+                                              (-<.>), (</>))
+import           System.Cmd                  (system)
 import           System.Console.CmdArgs
+import           System.Directory            (createDirectoryIfMissing)
 
 obj, un, dist :: FilePath -> FilePath
 obj = (".make" </>)
@@ -16,12 +21,14 @@ dist = ("dist" </>)
 data MkMode =
     Build
   | Preview
+  | Clean
   deriving (Show, Data, Typeable)
 
 mkModes :: MkMode
 mkModes = modes
   [ Build &= auto
   , Preview
+  , Clean
   ]
 
 newtype GhcPkg = GhcPkg ()
@@ -30,48 +37,75 @@ newtype GhcPkg = GhcPkg ()
 main :: IO ()
 main = do
   m <- cmdArgs mkModes
-  shake shakeOptions { shakeThreads = 2 } $ do
-    want [dist "manual/diagrams-manual.html"]
-    action $ requireIcons
-    action $ requireStatic
+  case m of
+    Clean -> mapM_ system
+      [ "rm -rf web/_site"
+      , "rm -rf web/_cache"
+      , "rm -f web/manual"
+      , "rm -f web/gallery/images"
+      , "rm -rf .make"
+      , "rm -rf dist"
+      , "rm -f Shake"
+      , "rm -f Shake.o"
+      , "rm -f Shake.hi"
+      , "rm .shake.database"
+      , "rm -f web/gallery/Build.{hi,o}"
+      ]
+    _ -> shake shakeOptions { shakeThreads = 2
+--                          , shakeVerbosity = Chatty
+                            } $ do
+      want [dist "manual/diagrams-manual.html"]
+      action $ requireIcons
+      action $ requireStatic
 
-    webRules
-    --action $ runWeb m
+      webRules
+      action $ runWeb m
 
-    -- Cheating a bit here; this rule also generates diagrams images,
-    -- BUT we don't even know their names until after running xml2html,
-    -- which generates diagrams-manual.html as well.  So we have to be
-    -- careful to call 'requireImages' *after* running xml2html.
-    dist "manual/diagrams-manual.html" *> \out -> do
-      let xml = obj . un $ out -<.> "xml"
-          exe = obj "manual/xml2html.hs.exe"
-      need [xml, exe]
-      system' exe [xml, "-o", obj "manual/images", out]
-      requireImages
+      -- Cheating a bit here; this rule also generates diagrams images,
+      -- BUT we don't even know their names until after running xml2html,
+      -- which generates diagrams-manual.html as well.  So we have to be
+      -- careful to call 'requireImages' *after* running xml2html.
+      dist "manual/diagrams-manual.html" *> \out -> do
+        let xml = obj . un $ out -<.> "xml"
+            exe = obj "manual/xml2html.hs.exe"
+        need [xml, exe]
+        system' exe [xml, "-o", obj "manual/images", out]
+        requireImages
 
-    obj "//*.xml" *> \out -> do
-      let rst = un $ out -<.> "rst"
-      need [rst]
-      system' "rst2xml" ["--input-encoding=utf8", rst, out]
+      obj "//*.xml" *> \out -> do
+        let rst = un $ out -<.> "rst"
+        need [rst]
+        system' "rst2xml" ["--input-encoding=utf8", rst, out]
 
-    obj "//*.hs.exe" *> \out -> do
-      let hs = un $ dropExtension out
-      need [hs]
-      ghc out hs
+      obj "//*.hs.exe" *> \out -> do
+        let hs = un $ dropExtension out
+        need [hs]
+        ghc out hs
 
-    dist "manual/icons/*.png" *> \out -> do
-      let exe = obj . un $ out -<.> ".hs.exe"
-      need [exe]
-      system' exe ["-w", "40", "-h", "40", "-o", out]
+      dist "manual/icons/*.png" *> \out -> do
+        let exe = obj . un $ out -<.> ".hs.exe"
+        need [exe]
+        system' exe ["-w", "40", "-h", "40", "-o", out]
 
-    copyFiles "manual/static"
-    copyImages
+      copyFiles "manual/static"
+      copyImages
 
-    addOracle $ \(GhcPkg _) -> do
-      (out,_) <- systemOutput "ghc-pkg" ["dump"]
-      return $ words out
+      dist "web/gallery/*.big.png" *> compilePng False
 
-    return ()
+      dist "web/gallery/*.thumb.png" *> compilePng True
+
+      addOracle $ \(GhcPkg _) -> do
+        (out,_) <- systemOutput "ghc-pkg" ["dump"]
+        return $ words out
+
+      return ()
+
+compilePng :: Bool -> FilePath -> Action ()
+compilePng isThumb outPath = do
+    systemCwd "web/gallery" ("../.." </> obj "web/gallery/Build.hs.exe")
+      ( (if isThumb then [ "--thumb", "175" ] else [])
+        ++ [(takeBaseName . takeBaseName) outPath, "../.." </> outPath]
+      )
 
 copyFiles :: String -> Rules ()
 copyFiles dir = dist (dir ++ "/*") *> \out -> copyFile' (un out) out
@@ -97,17 +131,34 @@ requireImages = do
   let distImages = map (dist . ("manual/images" </>)) images
   need distImages
 
+requireGallery :: Action ()
+requireGallery = do
+  gallerySrc <- filter (not . (".#" `isPrefixOf`))
+                <$> getDirectoryFiles "web/gallery" ["*.lhs"]
+  let pngs   = map (dist . ("web/gallery" </>) . (-<.> "big.png")) gallerySrc
+      thumbs = map (dist . ("web/gallery" </>) . (-<.> "thumb.png")) gallerySrc
+  need (pngs ++ thumbs)
+
+  -- XXX add rule to link them into the real web dir?
+
 webRules :: Rules ()
 webRules = do
   "web/manual" *> \out ->
     system' "ln" ["-s", "-f", "../dist/manual", out]
+
+  "web/gallery/images" *> \out -> do
+    liftIO $ createDirectoryIfMissing True "dist/web/gallery"
+    system' "ln" ["-s", "-f", "../../dist/web/gallery", out]
 
 runWeb :: MkMode -> Action ()
 runWeb m = do
   alwaysRerun
   needWeb
 
-  system' "rm" ["dist/manual/manual"] -- work around weird bug(?)
+  -- work around weird bug(?)
+  system' "rm" ["-f", "dist/manual/manual"]
+  system' "rm" ["-f", "dist/web/gallery/gallery"]
+
   systemCwd "web" (".." </> obj "web/hakyll.hs.exe")
     [ case m of
         Build   -> "build"
@@ -117,6 +168,7 @@ runWeb m = do
 needWeb :: Action ()
 needWeb = do
   need [ "web/manual"
+       , "web/gallery/images"
        , dist "manual/diagrams-manual.html"
        , obj "web/hakyll.hs.exe"
        , obj "web/gallery/Build.hs.exe"
@@ -124,8 +176,7 @@ needWeb = do
   requireIcons
   requireStatic
   requireImages
-
-  -- requireGallery  -- replace hakyll building with our own here
+  requireGallery  -- replace hakyll building with our own here
 
 ghc :: String -> String -> Action ()
 ghc out hs = do
--- web/hakyll.hs
+++ web/hakyll.hs
@@ -20,6 +20,19 @@ import           Text.Pandoc.Generic
 
 import           Hakyll
 
+pages, mdPages, lhsPages :: IsString s => [s]
+pages = mdPages ++ lhsPages
+
+mdPages = map (fromString . (++".markdown"))
+  [ "index"
+  , "download"
+  , "documentation"
+  , "community"
+  , "releases"
+  ]
+
+lhsPages = [ "tutorial/DiagramsTutorial.lhs" ]
+
 main :: IO ()
 main = hakyll $ do
     -- CSS, templates, JavaScript -----------------
@@ -35,20 +48,19 @@ main = hakyll $ do
 
     -- User manual --------------------------------
     match "manual/diagrams-manual.html" $ do
-      route idRoute
-      let manualCtx = constField "title" "User manual" `mappend` defaultContext
-      compile (getResourceBody >>= mainCompiler manualCtx)
+        route idRoute
+        let manualCtx = constField "title" "User manual" `mappend` defaultContext
+        compile (getResourceBody >>= mainCompiler manualCtx)
 
-    match ("manual/**" .&&. complement "manual/*.html" .&&. complement "manual/manual") $ do
-                         -- last pattern needed due to some sort of weird bug in shake?? =(
+    match ("manual/**" .&&. complement "manual/*.html") $ do
         route idRoute
         compile copyFileCompiler
 
     -- API documentation --------------------------
 
     match "doc/**" $ do
-      route idRoute
-      compile copyFileCompiler
+        route idRoute
+        compile copyFileCompiler
 
     -- Static images ------------------------------
 
@@ -67,41 +79,18 @@ main = hakyll $ do
 
     -- Example gallery ----------------------------
 
-      -- make all gallery/*.lhs available for building gallery.html
-    match "gallery/*.lhs" $ compile pandocCompiler
+    match "gallery/images/*.png" $ do
+        route idRoute
+        compile copyFileCompiler
 
-      -- build gallery.html from gallery.markdown and gallery/*.lhs
-      -- note the inGroup Nothing, which ensures we don't get
-      -- gallery/*.lhs resources from other groups, since we are using
-      -- gallery/*.lhs to build several different things
     match "gallery.markdown" $ do
         route $ setExtension "html"
 
         compile $ do
           galleryContent <- pandocCompiler
-          lhss <- loadAll ("gallery/*.lhs" .&&. hasNoVersion)
+          lhss <- loadAll ("gallery/*.lhs" .&&. hasVersion "gallery")
           gallery <- buildGallery galleryContent lhss
-          mainCompiler
-            ( mconcat
-              [ setHtmlURL
-              , setImgURL
-              , defaultContext
-              ]
-            )
-            gallery
-
-      -- generate .png from .lhs
-    match "gallery/*.lhs" $ version "png" $ do
-        route $ setExtension "png"
-        compile $ do
-          i <- getUnderlying
-          unsafeCompiler (compilePng False i)
-
-    match "gallery/*.lhs" $ version "png-thumb" $ do
-        route $ gsubRoute ".lhs" (const "-thumb.png")
-        compile $ do
-          i <- getUnderlying
-          unsafeCompiler (compilePng True i)
+          mainCompiler defaultContext gallery
 
       -- build syntax-highlighted source code for examples
     match "gallery/*.lhs" $ version "gallery" $ do
@@ -110,15 +99,15 @@ main = hakyll $ do
             >>= loadAndApplyTemplate "templates/exampleHi.html"
                   ( mconcat
                     [ setImgURL
+                    , setHtmlURL
 --                    , pandocFieldsCtx ["description"]
                     , defaultContext
                     ]
                   )
             >>= mainCompiler defaultContext
 
-      -- export raw .lhs of examples for download, stripping off the
-      -- metadata first
-    forM_ lhsPages $ \l -> match l $ version "raw" $ do
+      -- export raw .lhs of examples for download
+    match ("gallery/*.lhs" .||. fromList lhsPages) $ version "raw" $ do
         route idRoute
         compile getResourceBody
 
@@ -134,33 +123,22 @@ withMathJax =
           = RawInline "html" ("\\[" ++ str ++ "\\]")
         latexToMathJax x = x
 
-compilePng :: Bool -> Identifier -> IO (Item LB.ByteString)
-compilePng isThumb i = do
-    let thumbFlag | isThumb   = "--thumb 175 "
-                  | otherwise = ""
-    _ <- system $ "cd gallery && ../../.make/web/gallery/Build.hs.exe " ++ thumbFlag ++ moduleName ++ " " ++ tmpPath
-    Item i <$> LB.readFile tmpPath
-  where
-    moduleName = takeBaseName $ toFilePath i
-    tmpPath    = "/tmp/" ++ moduleName ++ (if isThumb then "-thumb" else "") ++ ".png"
-
 mainCompiler :: Context String -> Item String -> Compiler (Item String)
 mainCompiler ctx = loadAndApplyTemplate "templates/default.html" ctx
                >=> relativizeUrls
 
 setThumbURL, setImgURL, setHtmlURL :: Context String
-setThumbURL  = setURL "-thumb.png"
-setImgURL    = setURL "png"
-setHtmlURL   = setURL "html"
+setThumbURL  = setURL "images" "thumb.png"
+setImgURL    = setURL "images" "big.png"
+setHtmlURL   = setURL "" "html"
 
-setURL :: String -> Context String
-setURL ext = field (extNm ++ "url") fieldVal
+setURL :: FilePath -> String -> Context String
+setURL dir ext = field (extNm ++ "url") fieldVal
   where extNm = filter isAlphaNum ext
         fieldVal i = do
-          u <- getMetadataField' (itemIdentifier i) "url"
-          return (dropExtension u ++ ext')
-        ext' | '.' `elem` ext = ext
-             | otherwise      = '.' : ext
+          u <- fmap (maybe "" toUrl) . getRoute . itemIdentifier $ i
+          let (path,f) = splitFileName u
+          return (path </> dir </> replaceExtension f ext)
 
 -- pandocFieldsCtx :: [String] -> Context String
 -- pandocFieldsCtx = mconcat . map pandocFieldCtx
@@ -175,33 +153,24 @@ setURL ext = field (extNm ++ "url") fieldVal
 
 buildGallery :: Item String -> [Item String] -> Compiler (Item String)
 buildGallery content lhss = do
+  -- reverse sort by date (most recent first)
   lhss' <- mapM addDate lhss
-  exs   <- mapM compileExample . map snd . sortBy (comparing fst) $ lhss'
-  return (fmap (++ (concatMap itemBody exs)) content)
+  let exs = reverse . map snd . sortBy (comparing fst) $ lhss'
+
+      galleryCtx = mconcat
+        [ listField "examples" exampleCtx (return exs)
+        , defaultContext
+        ]
+      exampleCtx = mconcat
+        [ setHtmlURL
+        , setThumbURL
+        , defaultContext
+        ]
+
+  loadAndApplyTemplate "templates/gallery.html" galleryCtx content
+
   where
     addDate lhs = do
       d <- fromMaybe "" <$> getMetadataField (itemIdentifier lhs) "date"
       return (d,lhs)
 
-compileExample :: Item String -> Compiler (Item String)
-compileExample = loadAndApplyTemplate "templates/example.html"
-                 ( mconcat
-                   [ setHtmlURL
-                   , setThumbURL
-                   , defaultContext
-                   ]
-                 )
-
-pages, mdPages, lhsPages :: IsString s => [s]
-
-pages = mdPages ++ lhsPages
-
-mdPages = map (fromString . (++".markdown"))
-  [ "index"
-  , "download"
-  , "documentation"
-  , "community"
-  , "releases"
-  ]
-
-lhsPages = [ "tutorial/DiagramsTutorial.lhs", "gallery/*.lhs" ]
