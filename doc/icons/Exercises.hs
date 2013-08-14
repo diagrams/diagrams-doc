15600ab4d3ba32f27599d7d4e08aa9d565329fc3
Brent Yorgey <byorgey@cis.upenn.edu>
2013-08-14 11:14:33 -0400

    dumbbell for exercise logo

--- doc/icons/Exercises.hs
+++ doc/icons/Exercises.hs
@@ -5,4 +5,18 @@ module Exercises where
 import           Diagrams.Backend.Cairo.CmdLine
 import           Diagrams.Prelude
 
-main = defaultMain (pad 1.1 (text "e" # fc green <> square 1 # lw 0))
+weights :: [Double] -> Diagram Cairo R2
+weights hs
+  = hs
+  # map (\h -> roundedRect 1 h 0.2)
+  # hcat' with {sep = 0.3}
+
+dumbbell :: Double -> [Double] -> Diagram Cairo R2
+dumbbell w hs = hcat' with {sep = 0.5} [cap, wts, bar, wts # reflectX, cap]
+  where
+    wts = weights hs
+    cap = rect 0.8 1
+    bar = rect w 1
+
+main :: IO ()
+main = defaultMain (dumbbell 3 [4,5] # fc black # centerXY # pad 1.1)
