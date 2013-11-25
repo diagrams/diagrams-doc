189c60b02e845a9e5101967d88d58522cecffc6c
Brent Yorgey <byorgey@cis.upenn.edu>
2013-11-25 17:13:55 -0500

    NY Haskell talk

--- talks/2013-nyhaskell/Diagrams.hs
+++ talks/2013-nyhaskell/Diagrams.hs
@@ -2,14 +2,22 @@
 
 module Diagrams where
 
-import           Diagrams.Backend.Postscript.CmdLine
+import           Diagrams.Backend.Cairo.CmdLine
 import           Diagrams.Prelude
 
 dot = circle 0.1 # fc black # lw 0
+
+theTrail = fromOffsets [1 ^& 1, 1 ^& (-0.5), 1 ^& 0.25, 0.25 ^& 1, 1 ^& 0]
 
 showTrail :: Trail R2 -> Diagram B R2
 showTrail t =
   mconcat
   [ mconcat (map (place dot) (trailVertices (t `at` origin)))
-  , strokeTrail t
+  , strokeTrail t # lc green
   ]
+
+showTrailSegs :: Trail R2 -> Diagram B R2
+showTrailSegs t =
+  explodeTrail (t `at` origin)
+  # map (\l -> arrowBetween' (with & arrowShaft .~ unLoc l) (loc l) (loc l .+^ trailOffset (unLoc l)) # lc green)
+  # mconcat
