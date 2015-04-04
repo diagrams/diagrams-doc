2633ff1e506ee2f1ee3b0f1bb7d60fbd7b363ac0
Ryan Yates <fryguybob@gmail.com>
2015-04-04 14:24:08 -0400

    Avoid text for warning icon
    To ensure a centered "!" for all backends, make the "!" a diagram.

--- doc/icons/Warning.hs
+++ doc/icons/Warning.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE CPP                       #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleContexts          #-}
 
 module Warning where
 
@@ -14,9 +15,24 @@ signSide = hrule 1 <> arc (rotateBy (-1/4) xDir) (1/3 @@ turn) # scale 0.3
 
 sign = mconcat . take 3 . iterate (rotateBy (1/3)) $ signSide
 
+trapazoid :: (InSpace V2 n t, TrailLike t, OrderedField n)
+  => n -> n -> n -> t
+trapazoid h wt wb = trailLike $ ss `at` p2 (-wb/2, -h/2)
+  where
+    ss = closeTrail . trailFromSegments $ map (straight . r2)
+          [ (wb,0)
+          , ((wt-wb)/2,h)
+          , (-wt,0)
+          ]
+
+bang :: Diagram B
+bang = center $ vcat' (with & sep .~ 0.4) [trapazoid 3 1 0.8, circle 0.55]
+
 d :: Diagram B
-d = text "!" # fc black # bold # translate (-0.075 ^& 0)
- <> strokeLoop (glueLine sign) # lc black # lw (output 3) # center
+d = (bang # fc black # bold # pad 1.4 # sizedAs l) <> l
+  where
+    l :: Diagram B
+    l = strokeLoop (glueLine sign) # lc black # lw (output 3) # center
 
 
 main = defaultMain $ pad 1.15 d
