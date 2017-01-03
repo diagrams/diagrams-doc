48de50f53d3085d083ef21b6c4b6bab1c40fc88c
Brent Yorgey <byorgey@gmail.com>
2017-01-03 12:38:33 -0600

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-ghc --make Shake -threaded
-./Shake buildh
+stack build
+stack exec diagrams-doc -- +RTS -N4 -RTS buildh
 rsync -r web/_site/* byorgey@projects.haskell.org:/srv/projects/diagrams/
 ssh byorgey@projects.haskell.org 'chmod -R o+rX /srv/projects/diagrams/*'
