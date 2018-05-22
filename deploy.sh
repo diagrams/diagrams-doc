07489f514900011dbd14c64375efdaa1f1b2e299
Brent Yorgey <byorgey@gmail.com>
2018-05-22 14:16:31 -0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 stack build
 stack exec diagrams-doc -- +RTS -N4 -RTS buildh
-rsync -r web/_site/* byorgey@projects.haskell.org:/srv/projects/diagrams/
-ssh byorgey@projects.haskell.org 'chmod -R o+rX /srv/projects/diagrams/*'
+cp -R web/_site/* ../github.io/
+cd ../github.io/ && git commit -a && git push
