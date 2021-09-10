b4aaf21c689e96b1705f220c8b85cff3efbbc695
Brent Yorgey <byorgey@gmail.com>
2021-09-10 17:04:58 -0500

    update to LTS-18

--- generate-stack-yaml.hs
+++ generate-stack-yaml.hs
@@ -1,7 +1,7 @@
 #! /usr/bin/env stack
 {-
   stack runghc
-    --resolver lts-11.10
+    --resolver lts-18.8
 
     --package basic-prelude
     --package process
@@ -28,32 +28,44 @@ main = do
 
 mkYamlFile :: [Dep] -> Yaml.YamlBuilder
 mkYamlFile deps = Yaml.mapping
-  [ "resolver"   .= Yaml.string "lts-11.10"
-  , "extra-deps" .= (Yaml.array . map Yaml.string $
+  [ "resolver"   .= Yaml.string "lts-18.8"
+  , "extra-deps" .= Yaml.array (
+    (map Yaml.string
     [ "tuple-0.3.0.2"
-    , "SVGFonts-1.7"
-    , "cubicbezier-0.6.0.5"
+    , "cubicbezier-0.6.0.6"
     , "mfsolve-0.3.2.0"
-    , "haskell-src-exts-1.20.2"
-    , "haskell-src-exts-simple-1.20.0.0"
+    , "haskell-src-exts-1.23.1"
+    , "haskell-src-exts-simple-1.23.0.0"
     , "fast-math-1.0.2"
-    , "pandoc-2.2.1"
-    , "pandoc-types-1.17.4.2"
+    , "pandoc-2.14.2"
+    , "pandoc-types-1.22"
+    , "force-layout-0.4.0.6"
+    , "svg-builder-0.1.1"
+    , "hakyll-4.14.1.0"
+    , "palette-0.3.0.2"
+    , "active-0.2.0.15"
+    , "citeproc-0.5"
+    , "doctemplates-0.10"
+    , "skylighting-0.11"
+    , "skylighting-core-0.11"
     ])
-  , "packages"   .= Yaml.array
-      ( Yaml.string "."
-      : Yaml.mapping
-          [ "location" .= Yaml.mapping
-            [ "git"    .= Yaml.string "git@github.com:st3ll1s/haddock.git"
-            , "commit" .= Yaml.string "b3912d70f74b0693f1ea8cffb8f547b1303ef325"
-            ]
-          , "subdirs" .= Yaml.array
-            [ Yaml.string "haddock-library" ]
-          , "extra-dep" .= Yaml.bool True
-          ]
-      : map mkDepObject deps
-      )
+    ++
+    map mkDepObject deps
+    )
+  , "packages"   .= Yaml.array [Yaml.string "."]
   ]
+
+      -- : Yaml.mapping
+      --     [ "location" .= Yaml.mapping
+      --       [ "git"    .= Yaml.string "git@github.com:st3ll1s/haddock.git"
+      --       , "commit" .= Yaml.string "b3912d70f74b0693f1ea8cffb8f547b1303ef325"
+      --       ]
+      --     , "subdirs" .= Yaml.array
+      --       [ Yaml.string "haddock-library" ]
+      --     , "extra-dep" .= Yaml.bool True
+      --     ]
+      -- : map mkDepObject deps
+      -- )
 
   {- - location:
     git: git@github.com:st3ll1s/haddock.git
@@ -67,12 +79,9 @@ mkYamlFile deps = Yaml.mapping
     mkDepObject :: Dep -> Yaml.YamlBuilder
     mkDepObject (Dep name sha) =
       Yaml.mapping
-        [ "location" .= Yaml.mapping
-          [ "git"    .= Yaml.string ("https://github.com/diagrams/" <> name)
-          , "commit" .= Yaml.string sha
-          ]
-        , "extra-dep" .= Yaml.bool True
-        ]
+      [ "git"    .= Yaml.string ("https://github.com/diagrams/" <> name)
+      , "commit" .= Yaml.string sha
+      ]
 
 depsWithShas :: IO [Dep]
 depsWithShas =
@@ -82,8 +91,7 @@ depsWithShas =
 
 repoNames :: [Text]
 repoNames =
-  [ "active"
-  , "diagrams-builder"
+  [ "diagrams-builder"
   , "diagrams-haddock"
   , "diagrams-core"
   , "diagrams-lib"
@@ -94,4 +102,5 @@ repoNames =
   , "docutils"
   , "dual-tree"
   , "monoid-extras"
+  , "SVGFonts"
   ]
