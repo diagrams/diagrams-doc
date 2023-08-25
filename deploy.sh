8972ab036a175904ae5e51e3ad97bf0761916fe7
Brent Yorgey <byorgey@gmail.com>
2023-08-25 17:46:01 -0500

    update deploy script to add new images

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 stack build
 stack exec diagrams-doc -- +RTS -N4 -RTS buildh
 cp -R web/_site/* ../github.io/
-cd ../github.io/ && git commit -a && git push
+cd ../github.io/ && git add doc/images/* && git add blog/images* && git commit -a && git push
